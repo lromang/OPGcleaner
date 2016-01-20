8f4c1c9d856cf016963b04875def30ee1b95ef29
lromang <luis.roangarci@gmail.com>
2016-01-20 10:14:06 -0600

    modif

--- server.R
+++ server.R
@@ -1,31 +1,157 @@
+## Librerias
+library(gdata)
+library(dplyr)
+library(plyr)
+library(stringr)
+library(ggplot2)
+library(xlsx)
+
+clean_com <- function(word){
+    word <- iconv(word, "latin1", "ASCII", sub="_")
+    word  <- tolower(word)
+    eng   <- str_detect(word,"/")
+    if(eng == TRUE){
+        word  <- str_split(word, "/")[[1]][2]
+    }else{
+        word <- str_replace(word,"_","o")
+    }
+    word <- str_trim(word)
+    word <- str_split(word,"[[:space:]]")[[1]][1]
+    word
+}
+
 shinyServer(function(input, output) {
-    
+###############################################################
   datasetInput <- reactive({
     ## En esta seccin se carga la base de datos desde la liga que se proporciona.
-      if(input$link != "url"){
-          read.csv(input$link,encoding="UTF-8")
-      }
+          data <- data.frame(read.xls("http://tecnoregistro.com.mx/AGA2015/reportes/?action=exportarRegUser&email=marianacourtney@gmail.com&clave=4598cfd1cfe70d29e5c9d77b51002f272790ba66"))
+          data
   })
-  
+###############################################################
     output$table <- renderDataTable({
         ## Despliegue de resultados.
     datasetInput()
-  })
+    })
+###############################################################
+    output$plot <- renderPlot({
+        ## Despliegue de gráfica
+        data <- filter(datasetInput(),
+                      !is.na(as.Date(fecha_registro)))
+        ## Hacer fecha
+        data$fecha_registro <- as.Date(data$fecha_registro)
+        ## Procesar variable a desplegar
+        data[,15] <- laply(data[,15],
+                          function(t)t <- clean_com(t))
+        ## Poner nombres que pide carlos
+        data <- filter(data, comentarios %in% c("student",
+                                               "national",
+                                               "private",
+                                               "civil",
+                                               "other",
+                                               "legislative",
+                                               "academia",
+                                               "foundation",
+                                               "subnational",
+                                               "media",
+                                               "multilateral",
+                                               "judiciary"))
+
+        ## Serie de tiempo
+        if(input$graph != "Histograma"){
+            data_series <- plyr::count(data[,c(13,15)])
+             if(input$var == "General"){
+               plot <- ggplot(data = data_series,
+               aes(x = fecha_registro, y = freq, col = comentarios)
+               ) + geom_point() + geom_smooth(se = FALSE) +
+            theme(panel.background = element_blank(),
+                  axis.text.y = element_text(color="#2962ff", size =15),
+                  axis.title = element_blank())    +
+                   scale_color_discrete(labels = sort(c("Subnational Government",
+                                       "Student",
+                                       "Private Sector",
+                                       "Other",
+                                       "National Government",
+                                       "Multilateral Organization",
+                                       "Media",
+                                       "Legislative Branch",
+                                       "Judiciary Branch",
+                                       "Foundation or Donor",
+                                       "Civil Society Organization",
+                                       "Academia")))
+             }else{
+             plotVar <- str_split(tolower(input$var)," ")[[1]][1]
+             ## 
+             data_series     <- filter(data_series, comentarios == plotVar)
+                            plot <- ggplot(data = data_series,
+               aes(x = fecha_registro, y = freq)
+               ) + geom_point(color = "#2962ff") + geom_smooth(se = FALSE) +
+            theme(panel.background = element_blank(),
+                  axis.text.y = element_text(color="#2962ff", size =15),
+                  axis.title = element_blank())
+             }
+        }else{
+        ## Verificar la variable que está seleccionada
+        if(input$var == "General"){
+        plot_data <- plyr::count(data[,15])
+        plot      <- ggplot(data = plot_data,
+                           aes(x = x, y = freq)) +
+            geom_bar(stat = "identity",fill = "#2962ff") +
+            geom_text(aes(label = freq, y = freq + 25), col = "#2962ff", size = 5) +
+            theme(panel.background = element_blank(),
+                  axis.text.y = element_text(color="#2962ff", size =15),
+                  axis.title = element_blank()) +
+            scale_x_discrete(labels = sort(c("Subnational Government",
+                                       "Student",
+                                       "Private Sector",
+                                       "Other",
+                                       "National Government",
+                                       "Multilateral Organization",
+                                       "Media",
+                                       "Legislative Branch",
+                                       "Judiciary Branch",
+                                       "Foundation or Donor",
+                                       "Civil Society Organization",
+                                       "Academia")))+
+                         coord_flip()
+        }else{
+             country  <- iconv(data$residencia, "latin1", "ASCII", sub="_")
+             country  <- tolower(country)
+             country[country != "0" & country != "m_xico"] <- "other"
+             country[country == "m_xico"] <- "Mexico"
+             data_interest <- data.frame("comment" = data[,15], "residencia" = country)
+            ## Variable a graficar
+             plotVar <- str_split(tolower(input$var)," ")[[1]][1]
+             ## 
+             data_plot     <- filter(data_interest, comment == plotVar)
+             plot_data     <- plyr::count(data_plot[,2])
+             plot      <- ggplot(data = plot_data,
+                                aes(x = x, y = freq)) +
+                 geom_bar(stat = "identity",fill = "#2962ff") +
+                 geom_text(aes(label = freq, y = freq + 25), col = "#2962ff", size = 5) +
+                 theme(panel.background = element_blank(),
+                  axis.text.y = element_text(color="#2962ff", size =15),
+                  axis.title = element_blank()) +
+            coord_flip()
+        }
+        }
+        plot
+    })
   
 
   output$downloadData <- downloadHandler(
-    # Nombre del archivo
+    ## Nombre del archivo
     filename = function() {
 		  paste(input$nombre, input$filetype, sep = ".")
 	  },
 
-    # Esta fun. escribe el archivo.
+    ## Esta fun. escribe el archivo.
     content = function(file) {
-      sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t")
-
-      # Write to a file specified by the 'file' argument
-      write.table(datasetInput(), file, sep = sep,
-        row.names = FALSE)
+      sep <- switch(input$filetype, "csv" = ",", "xlsx" = "\t")
+        ## Write to a file specified by the 'file' argument
+      if(sep == ","){
+          write.table(datasetInput(), file, sep = sep,
+                  row.names = FALSE)
+      }
     }
   )
 })
--- ui.R
+++ ui.R
@@ -1,21 +1,92 @@
 shinyUI(fluidPage(
     titlePanel('OGP Cleaner'),
     theme="theme.css",
+    tags$head(tags$style(type="text/css", ".dataTables_filter {display: none;    }")),
   sidebarLayout(
-      sidebarPanel(
-          textInput("link", h4("Ingresa la URL de tus datos:"), value = "url"),
+      sidebarPanel(h3("Dashboard",style="color:#2962ff"),
+                   hr(),
+                   br(),
+###
+                   tabsetPanel(
+                       tabPanel("Análisis",
+                                br(),
+         radioButtons("graph", h4("Tipo de Gráfica:"),
+                                                   choices = c("Histograma","Serie de tiempo"),
+                                                   selected = "Histograma"),
+                                hr(),
+                                      radioButtons("var", h4("Variables:"),
+                                                   choices = c("General",
+                                                               "Academia",
+                                                               "Student",
+                                                               "Foundation or Donor",
+                                                               "National Government",
+                                                               "Subnational Government",
+                                                               "Private Sector",
+                                                               "Media",
+                                                               "Civil Society Organization",
+                                                               "Multilateral Organization",
+                                                               "Other",
+                                                               "Judiciary Branch",
+                                                               "Legislative Branch"),
+                                                   selected = "General")
+                                ),
+                   tabPanel("Descarga",
+                            br(),
            textInput("nombre", h4("Nombre archivo de descarga:"), value = "archivo"),
       radioButtons("filetype", "File type:",
-                   choices = c("csv", "tsv")),
+                   choices = c("csv")),
       downloadButton('downloadData', 'Download')
+      ),
+      tabPanel("Diccionario",
+               h5("Academia",style="color:#2962ff"),
+               p("Académico / Academia"),
+               hr(),
+               h5("Confirmación enviada",style="color:#2962ff"),
+               p("CONFIRMACIÓN ENVIADA"),
+               hr(),
+               h5("Student",style="color:#2962ff"),
+               p("Estudiante / Student"),
+               hr(),
+               h5("Foundation or Donor",style="color:#2962ff"),
+               p("Fundación o Donante / Foundation or Donor"),
+               hr(),
+               h5("National Government",style="color:#2962ff"),
+               p("Gobierno Nacional / National Government"),
+               hr(),
+               h5("Subnational Government",style="color:#2962ff"),
+               p("Gobierno Subnacional / Subnational Government"),
+                              hr(),
+               h5("Private Sector",style="color:#2962ff"),
+               p("Iniciativa Privada / Private Sector"),
+                              hr(),
+               h5("Media",style="color:#2962ff"),
+               p("Medios / Media"),
+                              hr(),
+               h5("Civil Society Organization",style="color:#2962ff"),
+               p("Organización de la Sociedad Civil / Civil Society Organization"),
+                                             hr(),
+               h5("Multilateral Organization",style="color:#2962ff"),
+               p("Organización Multilateral / Multilateral Organization"),
+                                             hr(),
+               h5("Other",style="color:#2962ff"),
+               p("Otro / Other"),
+                                             hr(),
+               h5("Judiciary Branch",style="color:#2962ff"),
+               p("Poder Judicial / Judiciary Branch"),
+                                                            hr(),
+               h5("Legislative Branch",style="color:#2962ff"),
+               p("Poder Legislativo / Legislative Branch")
+               )
+      )
+###
     ),
     mainPanel(
         tabsetPanel(
             tabPanel(h2("Datos"),
                 dataTableOutput('table')
             ),
-            tabPanel(h2("Analytics")
-                
+            tabPanel(h2("Analytics"),
+                plotOutput("plot")
             )
           )
     )
